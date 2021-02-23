f3d7be10dc04488cc02777c91b5510af415ac75d
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-23 10:17:27 +0000

    working on p1654

--- src/class02/p1654.cpp
+++ src/class02/p1654.cpp
@@ -1,22 +1,47 @@
 #include <cstdio>
+#include <algorithm>
+#include <vector>
+
+int divideCables(std::vector<int> &cables, int length)
+{
+    int totalCables = 0;
+    for (auto it : cables)
+        totalCables = totalCables + (it / length);
+    return totalCables;
+}
+int maxLength(std::vector<int> &cables, int count, int left, int right)
+{
+    if (left == right)
+        return left;
+    int mid = (left + right) / 2;
+    int midCount = divideCables(cables, mid);
+    if (midCount > count)
+        return maxLength(cables, count, mid + 1, right);
+    else if (midCount < count)
+        return maxLength(cables, count, left, mid - 1);
+    else
+    {
+        return left;
+    }
+}
 
 int main(void)
 {
-    int k, n;
+    std::vector<int> cables;
+    int k = 0, n = 0;
+
+    // Input
     scanf("%d %d", &k, &n);
+    for (int loop = 0, input = 0; loop < k; loop++)
+    {
+        scanf("%d", &input);
+        cables.push_back(input);
+    }
 
-    int *arr = new int[k]();
-    for (int loop = 0; loop < k; loop++)
-        scanf("%d", &arr[loop]);
+    // Sort in Ascending Order
+    std::sort(cables.begin(), cables.end());
 
-    int min = -1;
-    for (int loop = 0; loop < k; loop++)
-        if (min == -1)
-            min = arr[loop];
-        else if (arr[loop] < min)
-            min = arr[loop];
-    
-    
-
+    // Algorithm (Binary Search)
+    printf("%d\n", maxLength(cables, n, 1, cables.at(0)));
     return 0;
 }
