c53b6f53a9a475132b5c47de47c1098043bb4230
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-23 05:39:23 +0000

    solved p9015

--- src/class04/p9015.cpp
+++ src/class04/p9015.cpp
@@ -1,56 +1,41 @@
 #include <cstdio>
-#include <cmath>
 
-class Point
-{
-public:
-    int x = 0, y = 0;
-    Point() {}
-};
-
-int largestSquare(void)
-{
-    Point *array = nullptr;
-    int n = 0;
-    scanf("%d", &n);
-    array = new Point[n]();
-    for (int loop = 0; loop < n; loop++)
-        scanf("%d %d", &array[loop].x, &array[loop].y);
-    int maxSize = 0;
-    for (int l1 = 0; l1 < n - 1; l1++)
-    {
-        for (int l2 = l1 + 1; l2 < n; l2++)
-        {
-            Point p1 = array[l1], p2 = array[l2], p3, p4;
-            int x = p1.x - p2.x;
-            int y = p1.y - p2.y;
-            p3.x = p1.x + y;
-            p3.y = p1.y - x;
-            p4.x = p2.x + y;
-            p4.y = p2.y - x;
-
-            bool p3F = false, p4F = false;
-            for (int loop = 0; loop < n; loop++)
-                if (array[loop].x == p3.x && array[loop].y == p3.y)
-                    p3F = true;
-                else if (array[loop].x == p4.x && array[loop].y == p4.y)
-                    p4F = true;
-            if (p3F && p4F == true)
-            {
-                int area = pow((p2.y - p1.y), 2) + pow((p2.x - p1.x), 2);
-                if (maxSize < area)
-                    maxSize = area;
-            }
-        }
-    }
-    return maxSize;
-}
+double maximumAreaOfSquare(void);
 
 int main(void)
 {
-    int t = 0;
-    scanf("%d", &t);
-    for (int tLoop = 0; tLoop < t; tLoop++)
-        printf("%d\n", largestSquare());
+    int testCase = 0;
+    scanf("%d", &testCase);
+    for (int loop = 0; loop < testCase; loop++)
+        printf("%.0lf\n", maximumAreaOfSquare());
     return 0;
+}
+
+#include <cmath>
+#include <set>
+typedef std::pair<int, int> Point;
+double maximumAreaOfSquare(void)
+{
+    int numberOfPoints = 0;
+    double maxArea = 0.0F;
+    scanf("%d", &numberOfPoints);
+    Point pArray[numberOfPoints];
+    std::set<Point> pSet;
+    for (int loop = 0; loop < numberOfPoints; loop++)
+    {
+        scanf("%d %d", &pArray[loop].first, &pArray[loop].second);
+        pSet.insert(pArray[loop]);
+    }
+    for (int l1 = 0; l1 < numberOfPoints; l1++)
+        for (int l2 = l1 + 1; l2 < numberOfPoints; l2++)
+        {
+            Point p1 = pArray[l1], p2 = pArray[l2];
+            int x3 = p1.first + p1.second - p2.second;
+            int y3 = p1.second - p1.first + p2.first;
+            int x4 = p2.first + p1.second - p2.second;
+            int y4 = p2.second - p1.first + p2.first;
+            if ((pSet.find(Point(x3, y3)) != pSet.end()) && (pSet.find(Point(x4, y4)) != pSet.end()))
+                maxArea = std::max(maxArea, pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
+        }
+    return maxArea;
 }
