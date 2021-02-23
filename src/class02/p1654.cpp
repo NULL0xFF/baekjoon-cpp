7ac3704c3b19570f56ced8cf91b8b5b57ab6d1cc
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-23 11:13:45 +0000

    wrong p1654

--- src/class02/p1654.cpp
+++ src/class02/p1654.cpp
@@ -2,46 +2,46 @@
 #include <algorithm>
 #include <vector>
 
-int divideCables(std::vector<int> &cables, int length)
+long long divideCables(std::vector<long long> &cables, int length)
 {
-    int totalCables = 0;
+    long long totalCables = 0;
     for (auto it : cables)
         totalCables = totalCables + (it / length);
     return totalCables;
 }
-int maxLength(std::vector<int> &cables, int count, int left, int right)
+
+long long maxLength(std::vector<long long> &cables, int count, int left, int right)
 {
-    if (left == right)
-        return left;
     int mid = (left + right) / 2;
-    int midCount = divideCables(cables, mid);
-    if (midCount > count)
-        return maxLength(cables, count, mid + 1, right);
-    else if (midCount < count)
-        return maxLength(cables, count, left, mid - 1);
+    if (left == mid || mid == right)
+        return mid;
+    long long midCount = divideCables(cables, mid);
+    if (midCount < count)
+        return maxLength(cables, count, left, mid);
     else
-    {
-        return left;
-    }
+        return maxLength(cables, count, mid, right);
 }
 
 int main(void)
 {
-    std::vector<int> cables;
+    std::vector<long long> cables;
     int k = 0, n = 0;
 
     // Input
     scanf("%d %d", &k, &n);
-    for (int loop = 0, input = 0; loop < k; loop++)
     {
-        scanf("%d", &input);
-        cables.push_back(input);
+        long long input = 0L;
+        for (int loop = 0; loop < k; loop++)
+        {
+            scanf("%lld", &input);
+            cables.push_back(input);
+        }
     }
 
     // Sort in Ascending Order
     std::sort(cables.begin(), cables.end());
 
     // Algorithm (Binary Search)
-    printf("%d\n", maxLength(cables, n, 1, cables.at(0)));
+    printf("%lld\n", maxLength(cables, n, 1, cables.at(0)));
     return 0;
 }
