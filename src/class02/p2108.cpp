a589eb8485ea6253660f737497d327af95bb7ec0
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-26 08:21:17 +0000

    working on p2108

---
+++ src/class02/p2108.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(void)
+{
+    while(true)
+    {
+        // getchar() to get a string
+        // if strlen == 1 and string[0] == '.' then exit
+        // run func recursively to get result
+    }
+    return 0;
+}
