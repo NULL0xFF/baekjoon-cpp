f6deae7524f8a180e416f442f596d9287453ed13
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-27 13:43:04 +0900

    solved p2108

--- src/class02/p2108.cpp
+++ src/class02/p2108.cpp
@@ -1,26 +1,12 @@
 #include <cstdio>
 #include <cmath>
-#include <map>
 #include <algorithm>
+#include <map>
 
-int getMean(const int array[], int start, int end)
-{
-    int sum = 0;
-    for (int index = start - 1; index < end; index++)
-        sum = sum + array[index];
-    int result = sum / (end - start + 1);
-    if ((abs(sum) - abs(result * (end - start + 1)) >= 0.5) && sum < 0)
-        result = result - 1;
-    return result;
-}
-int getMedian(const int array[], int start, int end)
-{
-    return array[(end + start) / 2 - 1];
-}
 int getMode(const int array[], int start, int end)
 {
     std::map<int, int> modeMap;
-    for (int index = start - 1; index < end; index++)
+    for (int index = start; index <= end; index++)
     {
         auto it = modeMap.find(array[index]);
         if (it == modeMap.end())
@@ -44,22 +30,21 @@ int getMode(const int array[], int start, int end)
         }
     return mode;
 }
-int getRange(const int array[], int start, int end)
-{
-    return array[end - 1] - array[start - 1];
-}
 
 int main(void)
 {
-    int n = 0;
+    int n = 0, sum = 0;
     scanf("%d", &n);
-    int array[n] = {};
-    for (int loop = 0; loop < n; loop++)
-        scanf("%d", &array[loop]);
-    std::sort(array, array + n);
-    printf("%d\n", getMean(array, 1, n));
-    printf("%d\n", getMedian(array, 1, n));
+    int array[n + 1] = {};
+    for (int index = 1; index <= n; index++)
+    {
+        scanf("%d", &array[index]);
+        sum = sum + array[index];
+    }
+    printf("%d\n", (int)round((double)sum / (double)n)); // mean
+    std::sort(array + 1, array + (n + 1));
+    printf("%d\n", array[(int)round((double)n / (double)2)]);
     printf("%d\n", getMode(array, 1, n));
-    printf("%d\n", getRange(array, 1, n));
+    printf("%d\n", array[n] - array[1]);
     return 0;
 }
