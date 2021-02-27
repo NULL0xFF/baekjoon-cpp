94881e88cca0ba0fdad5bb7e6c441061928c0631
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-27 11:33:50 +0900

    solved p11651 with minor modification of p11650

---
+++ src/class02/p11651.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <algorithm>
+
+class Point
+{
+public:
+    int x, y;
+    Point() : Point(0, 0) {}
+    Point(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+};
+
+bool compare(const Point &a, const Point &b)
+{
+    if (a.y < b.y)
+        return true;
+    else if (a.y == b.y && a.x < b.x)
+        return true;
+    return false;
+}
+
+int main(void)
+{
+    int n = 0;
+    scanf("%d", &n);
+    Point coordinate[n];
+    for (int index = 0; index < n; index++)
+        scanf("%d %d", &coordinate[index].x, &coordinate[index].y);
+    std::sort(coordinate, coordinate + n, compare);
+    for (int index = 0; index < n; index++)
+        printf("%d %d\n", coordinate[index].x, coordinate[index].y);
+    return 0;
+}
