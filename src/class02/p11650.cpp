0e24fbb4bd49f74d2a34b8a8b1cd02ae60fa5ddd
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-27 11:32:32 +0900

    solved p11650 with implementation of Point class

--- src/class02/p11650.cpp
+++ src/class02/p11650.cpp
@@ -1,35 +1,36 @@
 #include <cstdio>
-#include <vector>
-#include <map>
 #include <algorithm>
+
+class Point
+{
+public:
+    int x, y;
+    Point() : Point(0, 0) {}
+    Point(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+};
+
+bool compare(const Point &a, const Point &b)
+{
+    if (a.x < b.x)
+        return true;
+    else if (a.x == b.x && a.y < b.y)
+        return true;
+    return false;
+}
 
 int main(void)
 {
-    std::map<int, std::vector<int> *> coordinationMap;
     int n = 0;
     scanf("%d", &n);
-    {
-        int a = 0, b = 0;
-        std::vector<int> *ptr = nullptr;
-        for (int loop = 0; loop < n; loop++)
-        {
-            scanf("%d %d", &a, &b);
-            if (coordinationMap.find(a) == coordinationMap.end())
-            {
-                ptr = new std::vector<int>();
-                ptr->push_back(b);
-                coordinationMap.insert(std::make_pair(a, ptr));
-                ptr = nullptr;
-            }
-            else
-                coordinationMap.find(a)->second->push_back(b);
-        }
-    }
-    for (auto element : coordinationMap)
-        std::sort(element.second->begin(), element.second->end(), std::less<int>());
-
-    for (auto element : coordinationMap)
-        for(auto point : *element.second)
-            printf("%d %d\n", element.first, point);
+    Point coordinate[n];
+    for (int index = 0; index < n; index++)
+        scanf("%d %d", &coordinate[index].x, &coordinate[index].y);
+    std::sort(coordinate, coordinate + n, compare);
+    for (int index = 0; index < n; index++)
+        printf("%d %d\n", coordinate[index].x, coordinate[index].y);
     return 0;
 }
