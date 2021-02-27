366db07bab9801e0438bf919fedc8f7e79a881a4
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-02-27 16:43:43 +0900

    solved p15829 using java without bigint

--- src/nonclass/P15829.java
+++ src/nonclass/P15829.java
@@ -1,10 +1,11 @@
 package nonclass;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class P15829 {
     public static void main(String[] args) {
+        final int mod = 1234567891;
+        final int mul = 31;
         Scanner inputStreamScanner = new Scanner(System.in);
         String inputString = "";
         int length = 0;
@@ -15,18 +16,13 @@ public class P15829 {
         inputStreamScanner.close();
 
         // Hashing
-        // if (length <= 5) {
-        //     long hash = 0;
-        //     for (int loop = 0; loop < length; loop++)
-        //         hash = hash + (((inputString.charAt(loop) - 'a') + 1) * (long) Math.pow(31, loop));
-        //     System.out.println(hash);
-        // } else {
-            BigInteger hash = BigInteger.ZERO;
-            for (int loop = 0; loop < length; loop++)
-                hash = hash.add(BigInteger.valueOf(31).pow(loop).multiply(BigInteger.valueOf((inputString.charAt(loop) - 'a') + 1)));
-            hash = hash.mod(BigInteger.valueOf(1234567891));
-            System.out.println(hash.toString());
-        // }
+        long hash = 0;
+        long r = 1;
+        for (int loop = 0; loop < length; loop++) {
+            hash = (hash + (((inputString.charAt(loop) - 'a') + 1) * r)) % mod;
+            r = (r * mul) % mod;
+        }
+        System.out.println(hash);
 
         // Finalize
         return;
