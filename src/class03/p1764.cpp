0940097f66e7b4773ec6304b6ff41812f0493a90
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-03-11 19:42:50 +0900

    solving p1764 (timeout)
    TODO use only one for-statement

--- src/class03/p1764.cpp
+++ src/class03/p1764.cpp
@@ -1,52 +1,33 @@
-#include <cstdio>
-#include <cstring>
+#include <iostream>
 #include <algorithm>
-#include <queue>
-
-bool compare(const char a[], const char b[]) { return strcmp(a, b) < 0; }
-
-bool search(char *array[], int start, int end, char target[])
-{
-    if (start > end)
-        return false;
-    int mid = (start + end) / 2;
-    int flag = strcmp(array[mid], target);
-    if (flag == 0)
-        return true;
-    else if (flag < 0)
-        return search(array, mid + 1, end, target);
-    else
-        return search(array, start, mid - 1, target);
-}
+#include <string>
+#include <vector>
 
 int main(void)
 {
-    char **listen = nullptr, **observe = nullptr;
+    std::vector<std::string> listen, observe, searched;
+    std::string s;
     int n = 0, m = 0;
-    scanf("%d %d", &n, &m);
-    listen = new char *[n]();
-    observe = new char *[m]();
+
+    std::cin >> n >> m;
+
     for (int loop = 0; loop < n; loop++)
     {
-        listen[loop] = new char[21]();
-        scanf(" %s", listen[loop]);
+        std::cin >> s;
+        listen.push_back(s);
     }
     for (int loop = 0; loop < m; loop++)
     {
-        observe[loop] = new char[21]();
-        scanf(" %s", observe[loop]);
+        std::cin >> s;
+        observe.push_back(s);
     }
-    std::sort(listen, listen + n, compare);
-    std::sort(observe, observe + m, compare);
-    std::queue<int> searched;
-    for (int index = 0; index < n; index++)
-        if (search(observe, 0, m, listen[index]) == true)
-            searched.push(index);
-    printf("%d\n", searched.size());
-    while (!searched.empty())
-    {
-        printf("%s\n", listen[searched.front()]);
-        searched.pop();
-    }
+    std::sort(listen.begin(), listen.end());
+    std::sort(observe.begin(), observe.end());
+    for (std::string cmp : listen)
+        if (std::find(observe.begin(), observe.end(), cmp) != observe.end())
+            searched.push_back(cmp);
+    std::cout << searched.size() << "\n";
+    for (std::string element : searched)
+        std::cout << element << "\n";
     return 0;
 }
