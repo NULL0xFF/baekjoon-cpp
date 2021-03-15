2b9c1bb206681dab2add09ba7c6da0eac887af0d
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-03-15 15:01:49 +0900

    solving p9019 timeout

--- src/class03/p9019.cpp
+++ src/class03/p9019.cpp
@@ -28,48 +28,56 @@
  * 예를 들어서 1000 에 L 을 적용하면 0001 이 되므로 결과는 1 이 된다.
  * 그러나 R 을 적용하면 0100 이 되므로 결과는 100 이 된다.
  */
-#include <cstdio>
+/* 
+ * 3
+ * 1234 3412
+ * 1000 1
+ * 1 16
+ */
+#include <iostream>
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <string>
 #include <utility>
-typedef std::pair<unsigned int, std::vector<unsigned int>> container;
-unsigned int fnD(const unsigned int n) { return (n * 2) % 10000; }
-unsigned int fnS(const unsigned int n) { return n != 0 ? n - 1 : 9999; }
-unsigned int fnL(const unsigned int n) { return (((n / 100) % 10) * 1000) + (((n / 10) % 10) * 100) + ((n % 10) * 10) + (n / 1000); }
-unsigned int fnR(const unsigned int n) { return ((n % 10) * 1000) + ((n / 1000) * 100) + (((n / 100) % 10) * 10) + ((n / 10) % 10); }
+typedef unsigned int uint;
+typedef std::pair<uint, std::string> container;
+uint fnD(const uint n) { return (n * 2) % 10000; }
+uint fnS(const uint n) { return n != 0 ? n - 1 : 9999; }
+uint fnL(const uint n) { return (((n / 100) % 10) * 1000) + (((n / 10) % 10) * 100) + ((n % 10) * 10) + (n / 1000); }
+uint fnR(const uint n) { return ((n % 10) * 1000) + ((n / 1000) * 100) + (((n / 100) % 10) * 10) + ((n / 10) % 10); }
 void dslr(void)
 {
-    unsigned int a = 0, b = 0;
-    scanf("%u %u", &a, &b);
+    uint a = 0, b = 0;
+    std::cin >> a >> b;
     // BFS
-    std::vector<unsigned int> visited;
+    std::vector<uint> visited;
     std::queue<container> queued;
-    queued.push(std::make_pair(a, std::vector<unsigned int>()));
+    queued.push(container(a, ""));
+    container con = {0, ""};
     while (!queued.empty())
     {
-        container reg = queued.front();
+        con = queued.front();
         queued.pop();
-        if (reg.first == b)
+        if (con.first == b)
             break;
-        if (std::find(visited.begin(), visited.end(), reg) == visited.end())
+        if (std::find(visited.begin(), visited.end(), con.first) == visited.end())
         {
-            visited.push_back(reg.first);
-            queued.push({
-                fnD(reg.first),
-            });
-            queued.push({fnS(reg), std::vector<unsigned int>(reg.second)});
-            queued.push(fnL(reg));
-            queued.push(fnR(reg));
+            visited.push_back(con.first);
+            queued.push(container(fnD(con.first), con.second + 'D'));
+            queued.push(container(fnS(con.first), con.second + 'S'));
+            queued.push(container(fnL(con.first), con.second + 'L'));
+            queued.push(container(fnR(con.first), con.second + 'R'));
         }
     }
+    std::cout << con.second << "\n";
     return;
 }
 int main(void)
 {
-    unsigned int t = 0;
-    scanf("%u", &t);
-    for (unsigned int loop = 0; loop < t; loop++)
+    uint t = 0;
+    std::cin >> t;
+    for (uint loop = 0; loop < t; loop++)
         dslr();
     return 0;
 }
