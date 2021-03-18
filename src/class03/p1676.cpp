6549480fee197df3efc26065b2a6f88130456056
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-03-18 20:52:40 +0900

    solved p1676

--- src/class03/p1676.cpp
+++ src/class03/p1676.cpp
@@ -1,33 +1,13 @@
 #include <cstdio>
 
-typedef unsigned long long ull;
-typedef unsigned int ui;
-ull factorial(ui n)
-{
-    if (n < 0)
-        return 0;
-    if (n == 0 || n == 1)
-        return 1;
-    ull ret = 1;
-    for (ui loop = 2; loop <= n; loop++)
-        ret = ret * loop;
-    return ret;
-}
-
 int main(void)
 {
-    ui n = 0;
-    scanf("%u", &n);
-    ull fact = factorial(n);
-    printf("%llu\n", fact);
-    ull count = 0;
-    while (fact != 0)
-    {
-        if (fact % 10 != 0)
-            break;
-        count++;
-        fact = fact / 10;
-    }
-    printf("%llu\n", count);
+    int n = 0, two = 0, five = 0;
+    scanf("%d", &n);
+    for (int i = 2; i <= n; i *= 2)
+        two += n / i;
+    for (int i = 5; i <= n; i *= 5)
+        five += n / i;
+    printf("%u\n", five > two ? two : five);
     return 0;
 }
