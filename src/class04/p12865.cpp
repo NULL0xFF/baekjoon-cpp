b360824d190b2e09e1e41bf276918d675ef5baef
Ji Myoung Ha <NoPlayer40600@gmail.com>
2021-03-22 16:54:49 +0900

    solved p12865

--- src/class04/p12865.cpp
+++ src/class04/p12865.cpp
@@ -1,53 +1,22 @@
 #include <cstdio>
-#include <algorithm>
-#include <vector>
 
-class Item
-{
-public:
-    int weight, value;
-    Item(int w = 0, int v = 0) : weight(w), value(v) {}
-};
+int max(int a, int b) { return a > b ? a : b; }
 
 int main(void)
 {
-    int n = 0, k = 0;
-    scanf("%d %d", &n, &k);
-    Item items[n + 1] = {};
-    int **data = new int *[n + 1]();
-    for (int index = 0; index <= n; index++)
-        data[index] = new int[k + 1]();
-    for (int index = 1; index <= n; index++)
-    {
-        scanf("%d %d", &items[index].weight, &items[index].value);
-    }
-    for (int w = 0; w <= k; w++)
-        data[0][w] = 0;
-    for (int i = 1; i <= n; i++)
-        data[i][0] = 0;
-    for (int i = 1; i <= n; i++)
-        for (int w = 0; w <= k; w++)
-            if (items[i].weight <= w)
-                if (items[i].value + data[i - 1][w - items[i].weight] > data[i - 1][w])
-                    data[i][w] = items[i].value + data[i - 1][w - items[i].weight];
-                else
-                    data[i][w] = data[i - 1][w];
+    int numberOfItems = 0, capacity = 0;
+    scanf("%d %d", &numberOfItems, &capacity);
+    int weight[numberOfItems + 1] = {};
+    int value[numberOfItems + 1] = {};
+    for (int index = 1; index <= numberOfItems; index++)
+        scanf("%d %d", &weight[index], &value[index]);
+    int data[numberOfItems + 1][capacity + 1] = {};
+    for (int i = 1; i <= numberOfItems; i++)
+        for (int j = 0; j <= capacity; j++)
+            if (weight[i] > j)
+                data[i][j] = data[i - 1][j];
             else
-                data[i][w] = data[i - 1][w];
-    std::vector<int> selected;
-    while (n > 0 && k > 0)
-    {
-        if (data[n][k] != data[n - 1][k])
-        {
-            selected.push_back(n);
-            n--;
-            k = k - items[n].weight;
-        }
-        else
-        {
-            n--;
-        }
-    }
-    printf("done");
+                data[i][j] = max(data[i - 1][j], data[i - 1][j - weight[i]] + value[i]);
+    printf("%d\n", data[numberOfItems][capacity]);
     return 0;
 }
